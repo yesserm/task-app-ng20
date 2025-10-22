466d76ff40494eb38ab3eaac9ac379baf5078182
yesserm <yesser.miranda0106@gmail.com>
2025-10-21 23:16:57 -0600

    feat(dashboard|tasks) - Add dashboard and task item component with services

---
+++ src/app/features/tasks/task-item/task-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  imports: [],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+
+}
